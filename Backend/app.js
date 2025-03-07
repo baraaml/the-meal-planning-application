168ea111029d706f816ff31e6f6b3d25334a024f
Amr Khaled <amr287491@gmail.com>
2025-03-08 01:39:42 +0200

    Created jobs dir and moved cron inside it

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("express-async-errors");
 require("dotenv").config();
-require("./utils/cron"); // This will automatically start the cleanup job
+require("./jobs/cron"); // This will automatically start the cleanup job
 const { PrismaClient } = require("@prisma/client");
 const cors = require("cors");
 const path = require("path");
