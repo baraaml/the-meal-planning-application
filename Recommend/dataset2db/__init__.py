9aec421c8446eafc9eb349b63a61475f0c770762
1AbdulkarimMousa <abdulkareem.oe@gmail.com>
2025-04-12 18:51:00 +0000

    bundling recommendation

---
+++ Recommend/dataset2db/__init__.py
@@ -0,0 +1,8 @@
+"""
+Dataset2DB Package
+
+This package handles loading recipe data from CSV and JSON files 
+into the MealFlow database. It preserves the data structure while
+transforming it to match the database schema.
+"""
+
